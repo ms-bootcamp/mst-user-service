9d4f5e660cb01d57722493a9680b92866a06fa3f
YuanShenjian <sjyuan@thoughtworks.com>
2018-01-29 10:24:45 +0800

    Reversion gradle

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 
-docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/opt/app -w /opt/app gradle:4.4.1-jdk8 gradle clean bootRepackage
+docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/opt/app -w /opt/app gradle:3.5-jdk8 gradle clean bootRepackage
 if [[ -z $DOCKER_REGISRTY ]]; then
   DOCKER_REGISRTY=10.202.129.3:5000
 fi
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 set -x
 set -e
 
-docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/opt/app -w /opt/app gradle:4.4.1-jdk8 gradle clean test
+docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/opt/app -w /opt/app gradle:3.5-jdk8 gradle clean test
